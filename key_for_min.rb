b265cafd37fa7ac8728c83942dbc2eb6006c4f57
Chao Hsuan Chi <allen0413@gmail.com>
2018-11-15 03:06:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,7 @@ def key_for_min_value(name_hash)
   item = ""
   placeholder = 1000000
   name_hash.each {|key, value|
-    if key = ""
-      return nil
-    elsif value < placeholder
+    if value < placeholder
       item = key
       placeholder = value
     end
